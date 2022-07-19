09586d8fac58b1144ed3869680f7254fac725152
holazz <2418184580@qq.com>
2022-07-19 09:31:08 +0800

    chore: expose `NormalizedWheelEvent`

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,11 @@
+export interface NormalizedWheelEvent {
+  spinX: number,
+  spinY: number,
+  pixelX: number,
+  pixelY: number
+}
+
 declare module 'normalize-wheel-es' {
-    interface NormalizedWheelEvent {
-        spinX: number,
-        spinY: number,
-        pixelX: number,
-        pixelY: number
-    }
-    function normalizeWheel(e: WheelEvent): NormalizedWheelEvent;
-    export = normalizeWheel;
+  function normalizeWheel(e: WheelEvent): NormalizedWheelEvent;
+  export = normalizeWheel;
 }
